67626119cf4bfaf93c0ccb4ae9d4262af106c77d
Sabrina Bea <me@sabrinabea.com>
2023-08-22 11:02:48 -0400

    Initial Commit

---
+++ Balance/BalanceApp.swift
@@ -0,0 +1,17 @@
+//
+//  BalanceApp.swift
+//  Balance
+//
+//  Created by Sabrina Bea on 8/22/23.
+//
+
+import SwiftUI
+
+@main
+struct BalanceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
